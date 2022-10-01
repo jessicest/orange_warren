51c2f8285afabe5565ca6a12bb58408608fb11c3
Jessica Knight <corence@gmail.com>
2022-10-01 18:49:10 +1000

    yay! info panel now updates with the selected unit's id

--- src/world_view.rs
+++ src/world_view.rs
@@ -1,35 +1,40 @@
 
-use std::cell::RefCell;
+use std::borrow::Borrow;
+use std::cell::{RefCell, Ref};
 use std::rc::Rc;
 
 use crate::fragment::{UnitId, Shard::*, Zone, Fragment};
 use crate::world::World;
-use druid::widget::{Align, Flex, Label, Padding, Painter, Controller, Scroll};
-use druid::{AppLauncher, Color, RenderContext, PlatformError, Widget, WindowDesc, PaintCtx, WidgetExt, Env, EventCtx, Event};
+use druid::widget::{Flex, Label, Padding, Painter, Controller, Scroll};
+use druid::{AppLauncher, Color, RenderContext, PlatformError, Widget, WindowDesc, PaintCtx, WidgetExt, Env, EventCtx, Event, MouseButton, Data, lens};
 
-type WorldData = Rc<RefCell<WorldView>>;
+type TheWorld = Rc<RefCell<World>>;
 
+#[derive(Clone, Data)]
 struct WorldView {
-    world: World,
+    world: TheWorld,
+    selected_unit_id: Option<UnitId>,
 }
 
 impl WorldView where {
-    fn new(world: World) -> Self {
+    fn new(world: TheWorld) -> Self {
         WorldView {
             world,
+            selected_unit_id: None,
         }
     }
 
     fn step(&mut self, unit_id: &str, x: i64, y: i64) {
-        let fragment = self.world.fragments
+        let mut world = self.world.borrow_mut();
+        let fragment = world.fragments
             .get_all(unit_id, "UnitIsInZone")
             .find(|f| matches!(f.shard, UnitIsInZone(_)))
             .expect("avatar should exist")
             .clone();
-        self.world.fragments.remove(&fragment);
+        world.fragments.remove(&fragment);
         if let UnitIsInZone(Zone(zx, zy, 1)) = fragment.shard {
             let zone = Zone(zx + x, zy + y, 1);
-            self.world.fragments.add(Fragment::new(
+            world.fragments.add(Fragment::new(
                 unit_id,
                 &format!("{:?}", zone),
                 &fragment.shard_name,
@@ -48,14 +53,13 @@ impl KeyController {
     }
 }
 
-impl <Child: Widget<WorldData>> Controller<WorldData, Child> for KeyController {
-    fn event(&mut self, child: &mut Child, ctx: &mut EventCtx, event: &Event, data: &mut WorldData, env: &Env) {
+impl <Child: Widget<WorldView>> Controller<WorldView, Child> for KeyController {
+    fn event(&mut self, child: &mut Child, ctx: &mut EventCtx, event: &Event, world_view: &mut WorldView, env: &Env) {
         use druid::Code::*;
 
         match &event {
             Event::WindowConnected => ctx.request_focus(),
             Event::KeyUp(key_event) => {
-                let mut world_view = data.borrow_mut();
                 match key_event.code {
                     Numpad1 => world_view.step("player", -1, 1),
                     Numpad2 => world_view.step("player", 0, 1),
@@ -69,27 +73,98 @@ impl <Child: Widget<WorldData>> Controller<WorldData, Child> for KeyController {
                 }
                 ctx.request_paint();
             },
-            _ => child.event(ctx, event, data, env),
+            _ => child.event(ctx, event, world_view, env),
         }
     }
 }
 
-fn build_ui() -> impl Widget<WorldData> {
+struct RepaintOnClick {
+}
+
+impl RepaintOnClick {
+    fn new() -> Self {
+        RepaintOnClick {
+        }
+    }
+}
+
+impl <Child: Widget<WorldView>> Controller<WorldView, Child> for RepaintOnClick {
+    fn event(&mut self, child: &mut Child, ctx: &mut EventCtx, event: &Event, data: &mut WorldView, env: &Env) {
+        match &event {
+            Event::MouseUp(_) | Event::MouseDown(_) => { ctx.request_update(); ctx.request_paint(); },
+            _ => (),
+        }
+        child.event(ctx, event, data, env);
+    }
+}
+
+struct ClickSelector {
+    offset: (i64, i64),
+}
+
+impl ClickSelector {
+    fn new(offset: (i64, i64)) -> Self {
+        ClickSelector {
+            offset,
+        }
+    }
+}
+
+impl <Child: Widget<WorldView>> Controller<WorldView, Child> for ClickSelector {
+    fn event(&mut self, child: &mut Child, ctx: &mut EventCtx, event: &Event, world_view: &mut WorldView, env: &Env) {
+        match &event {
+            Event::MouseUp(mouse_event) => {
+                if mouse_event.button == MouseButton::Left {
+                    let mut new_unit_id = None;
+                    let world = world_view.world.borrow_mut();
+
+                    for player_fragment in world.fragments.get_all("player", "UnitIsInZone") {
+                        if let &UnitIsInZone(player_zone) = &player_fragment.shard {
+                            let zid = player_zone.adjust(self.offset.0, self.offset.1);
+                            for fragment in world.fragments.get_all(&format!("{:?}", zid), "UnitIsInZone") {
+                                if let UnitIsInZone(_) = fragment.shard {
+                                    new_unit_id = Some(fragment.a.clone());
+                                }
+                            }
+                        }
+                    }
+
+                    world_view.selected_unit_id = new_unit_id;
+                    ctx.request_paint();
+                }
+            },
+            _ => child.event(ctx, event, world_view, env),
+        }
+    }
+}
+
+fn build_ui() -> impl Widget<WorldView> {
     Padding::new(
         10.0,
         Flex::row()
             .with_flex_child(make_viewport_widget(), 1.0)
-            .with_flex_child(Scroll::new(Label::new("no unit selected")).vertical(), 1.0),
+            .with_flex_child(Scroll::new(make_info_panel()).vertical(), 1.0)
+            .controller(RepaintOnClick::new())
     )
 }
 
+fn make_info_panel() -> impl Widget<WorldView> {
+    Label::new(|selected_unit_id: &Option<String>, _env: &_| {
+        if let Some(uid) = &selected_unit_id {
+            uid.clone()
+        } else {
+            String::from("no unit selected")
+        }
+    }).lens(lens!(WorldView, selected_unit_id))
+}
+
 pub fn do_a_window(world: World) -> Result<(), PlatformError> {
-    let world_view = Rc::new(RefCell::new(WorldView::new(world)));
+    let world_view = WorldView::new(Rc::new(RefCell::new(world)));
     AppLauncher::with_window(WindowDesc::new(build_ui())).launch(world_view)?;
     Ok(())
 }
 
-fn make_viewport_widget() -> impl Widget<WorldData> {
+fn make_viewport_widget() -> impl Widget<WorldView> {
     let mut grid = Flex::column();
     for y in (-4)..=(4) {
         let mut row = Flex::row();
@@ -103,26 +178,31 @@ fn make_viewport_widget() -> impl Widget<WorldData> {
         .controller(KeyController::new())
 }
 
-fn make_cell_widget(offset: (i64, i64)) -> impl Widget<WorldData> {
-    Painter::new(move |ctx, world_view: &WorldData, env| {
-        let world = &world_view.borrow().world;
+fn make_cell_widget(offset: (i64, i64)) -> impl Widget<WorldView> {
+    Painter::new(move |ctx, world_view: &WorldView, _env| {
+        let world = world_view.world.borrow_mut();  // todo: idk why .borrow() doesn't work here
+        let selected_unit_id = &world_view.selected_unit_id;
 
         for player_fragment in world.fragments.get_all("player", "UnitIsInZone") {
             if let &UnitIsInZone(player_zone) = &player_fragment.shard {
                 let zid = player_zone.adjust(offset.0, offset.1);
                 for fragment in world.fragments.get_all(&format!("{:?}", zid), "UnitIsInZone") {
                     if let UnitIsInZone(_) = fragment.shard {
-                        paint_unit(ctx, world, &fragment.a);
+                        paint_unit(ctx, &world, &fragment.a, selected_unit_id);
                     }
                 }
             }
         }
-    })
+    }).controller(ClickSelector::new(offset))
 }
 
-fn paint_unit<'a, 'b, 'c>(ctx: &mut PaintCtx<'a, 'b, 'c>, world: &World, uid: &UnitId) {
+fn paint_unit<'a, 'b, 'c>(ctx: &mut PaintCtx<'a, 'b, 'c>, world: &World, uid: &str, selected_unit_id: &Option<UnitId>) {
     let bounds = ctx.size().to_rect().inset(-4.0);
     let rounded = bounds.to_rounded_rect(3.0);
     ctx.fill(rounded, &Color::LIME);
-    ctx.stroke(rounded, &Color::BLUE, 2.0);
+    if let Some(selected_unit_id) = selected_unit_id {
+        if selected_unit_id == uid {
+            ctx.stroke(rounded, &Color::BLUE, 2.0);
+        }
+    }
 }
