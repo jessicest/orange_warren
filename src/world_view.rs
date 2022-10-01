f924fcd4a1878be1489284ab889cfa9692f2bab5
Jessica Knight <corence@gmail.com>
2022-10-01 17:36:22 +1000

    create new layout for viewport and info panels. also, fix numpad1 move direction

--- src/world_view.rs
+++ src/world_view.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 
 use crate::fragment::{UnitId, Shard::*, Zone, Fragment};
 use crate::world::World;
-use druid::widget::{Align, Flex, Label, Padding, Painter, Controller};
+use druid::widget::{Align, Flex, Label, Padding, Painter, Controller, Scroll};
 use druid::{AppLauncher, Color, RenderContext, PlatformError, Widget, WindowDesc, PaintCtx, WidgetExt, Env, EventCtx, Event};
 
 type WorldData = Rc<RefCell<WorldView>>;
@@ -78,18 +78,8 @@ fn build_ui() -> impl Widget<WorldData> {
     Padding::new(
         10.0,
         Flex::row()
-            .with_flex_child(
-                Flex::column()
-                    .with_flex_child(Label::new("top left"), 1.0)
-                    .with_flex_child(Align::centered(Label::new("bottom left")), 1.0),
-                1.0,
-            )
-            .with_flex_child(
-                Flex::column()
-                    .with_flex_child(Label::new("top right"), 1.0)
-                    .with_flex_child(Align::centered(make_viewport_widget()), 1.0),
-                1.0,
-            ),
+            .with_flex_child(make_viewport_widget(), 1.0)
+            .with_flex_child(Scroll::new(Label::new("no unit selected")).vertical(), 1.0),
     )
 }
 
